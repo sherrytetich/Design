a714e57884933d5e7561a50501302377714bcf45
sherrytetich <sheryl@kabarak.ac.ke>
2023-04-01 22:47:04 +0300

    pie chart changes

--- index.php
+++ index.php
@@ -1,48 +1,56 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "ussdsecontrial");
-
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-
-$sql = "SELECT location, SUM(registered) AS total_dependants
-        FROM approved
-        GROUP BY location";
-$result = mysqli_query($conn, $sql);
-
-$data = array();
-while ($row = mysqli_fetch_assoc($result)) {
-  $data[] = array($row["location"], (int) $row["total_dependants"]);
-}
-
-mysqli_close($conn);
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="UTF-8">
+  <title>Dependants by Location</title>
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <style>
+    body{
+      background-color: #aecdc2;
+    }
+    canvas {
+      display: block;
+      margin: 0 auto;
+      background-color: #aecdc2;
+      margin-top: 30px;
+    }
+  </style>
 </head>
 <body>
-  <canvas id="myChart"></canvas>
+  <?php
+    $conn = mysqli_connect("localhost", "root", "", "ussdsecontrial");
+
+    // Check connection
+    if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $sql = "SELECT location, SUM(registered) AS total_dependants
+            FROM approved
+            GROUP BY location";
+    $result = mysqli_query($conn, $sql);
+
+    $data = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+      $data[] = array($row["location"], (int) $row["total_dependants"]);
+    }
+
+    mysqli_close($conn);
+
+    $colors = ['#004c6d','#006d90','#008fb1','#00b3cf','#00d9e9','#00ffff'];
+  ?>
+  <canvas id="myChart" width="600" height="600"></canvas>
+  
+  </div>
   <script>
-    // Replace the data array with the array created above
     var data = <?php echo json_encode($data); ?>;
-
-    // Create an array of colors for the chart
-    var colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange'];
-
-    // Create an array of labels and an array of data values from the data array
+    var colors = <?php echo json_encode($colors); ?>;
     var labels = [];
     var values = [];
     for (var i = 0; i < data.length; i++) {
       labels.push(data[i][0]);
       values.push(data[i][1]);
     }
-
-    // Create a pie chart with Chart.js
     var ctx = document.getElementById('myChart').getContext('2d');
     var chart = new Chart(ctx, {
       type: 'pie',
@@ -52,6 +60,10 @@ mysqli_close($conn);
           backgroundColor: colors,
           data: values
         }]
+      },
+      options: {
+        responsive: false,
+        maintainAspectRatio: false
       }
     });
   </script>
